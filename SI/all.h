51d01291746e143e9945ab7566cb17d135df6dc6
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-03-01 16:18:09 +0100

    Updated SI/all.h

--- SI/all.h
+++ SI/all.h
@@ -1,10 +1,11 @@
-// <SI/all.h> - includes all other header files (for convenience)
+// <SI/all.h> - includes all SI lib's header files (for convenience)
 #pragma once
 
-#include "datatypes.h" // <-- type-safe SI datatypes such as SI::length or SI::time
-#include "units.h"     // <-- type-safe units such as SI::meters or SI::seconds
-#include "constants.h" // <-- type-safe constants such as SI::constant::speed_of_light
-#include "literals.h"  // <-- convenient literals such as 100_m or 60_sec
-#include "formulas.h"  // <-- common formulas such as SI::formula::wavelength()
-#include "IO.h"        // <-- input/output functions such as SI::print()
-#include "tests.h"     // <-- unit tests at compile-time to verify everything
+#include "datatypes.h" // <-- Datatypes such as SI::length or SI::time
+#include "units.h"     // <-- Units such as SI::meters or SI::seconds
+#include "literals.h"  // <-- Literals such as 100_m or 60_s
+#include "constants.h" // <-- Constants such as SI::constant::speed_of_light
+#include "formulas.h"  // <-- Formulas such as SI::formula::wavelength()
+#include "conversion.h"// <-- Conversion functions such as SI::to_string()
+#include "IO.h"        // <-- I/O functions such as SI::print()
+#include "tests.h"     // <-- Unit tests at compile-time to verify everything
