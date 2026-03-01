ba1a39a677629019943a4494dbfbe1b4500cf6c9
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-03-01 18:19:38 +0100

    Updated examples.cpp

--- examples.cpp
+++ examples.cpp
@@ -300,6 +300,6 @@ int main() {
 	dimensionless y = t / 1_s; // <- y again contains a dimensionless number (no unit)
 	// NOTE: this does not work for celsius and fahrenheit due to the offset!
 }
-	print("\nNOTE: Use a modern Unicode terminal in case of gibberish characters!");
+	print("\nHINT: In case of gibberish characters use a modern terminal with Unicode support!");
 	return 0;
 }
