a20b90d2e9307ebde8373501602e6b70d2fc8000
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-03-13 18:02:34 +0100

    Improved SI/IO.h

--- SI/IO.h
+++ SI/IO.h
@@ -1,49 +1,50 @@
-// <SI/IO.h> - I/O functions to print single and multiple SI datatypes, equivalents, and text.
+// <SI/IO.h> - I/O functions to print SI datatypes, equivalents, and text.
 #pragma once
 #include <iostream>
 #include <SI/to_string.h>
 
 namespace SI
 {
-	// the internal print function
-	void _print(const std::string& text)
-	{
-		std::cout << text;
-	}
-
-	// print a single SI value
+	// print a single SI datatype
 	template <typename T>
 	void print(T value)
 	{
-		_print(to_string(value));
+		std::cout << to_string(value);
 	}
 
-	// print 2 SI values
+	// print two SI datatypes
 	template <typename T1, typename T2>
 	void print(T1 v1, T2 v2)
 	{
-		_print(to_string(v1) + ", " + to_string(v2));
+		std::cout << to_string(v1) << ", " << to_string(v2);
 	}
 
-	// print 3 SI values
+	// print 3 SI datatypes
 	template <typename T1, typename T2, typename T3>
 	void print(T1 v1, T2 v2, T3 v3)
 	{
-		_print(to_string(v1) + ", " + to_string(v2) + ", " + to_string(v3));
+		std::cout << to_string(v1) << ", " << to_string(v2) << ", " << to_string(v3);
 	}
 
-	// print 4 SI values
+	// print 4 SI datatypes
 	template <typename T1, typename T2, typename T3, typename T4>
 	void print(T1 v1, T2 v2, T3 v3, T4 v4)
 	{
-		_print(to_string(v1) + ", " + to_string(v2) + ", " + to_string(v3) + ", " + to_string(v4));
+		std::cout << to_string(v1) << ", " << to_string(v2) << ", " << to_string(v3) << ", " << to_string(v4);
 	}
 
-	// print 5 SI values
+	// print 5 SI datatypes
 	template <typename T1, typename T2, typename T3, typename T4, typename T5>
 	void print(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5)
 	{
-		_print(to_string(v1) + ", " + to_string(v2) + ", " + to_string(v3) + ", " + to_string(v4) + ", " + to_string(v5));
+		std::cout << to_string(v1) << ", " << to_string(v2) << ", " << to_string(v3) << ", " << to_string(v4) << ", " << to_string(v5);
+	}
+
+	// print 6 SI datatypes
+	template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+	void print(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6)
+	{
+		std::cout << to_string(v1) << ", " << to_string(v2) << ", " << to_string(v3) << ", " << to_string(v4) << ", " << to_string(v5) << ", " << to_string(v6);
 	}
 
 } // namespace SI
