649612aae3e9f0a117eae0fd567d64c9cd039fa1
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-03-16 09:33:13 +0100

    cosmetic changes

--- SI/datatypes.h
+++ SI/datatypes.h
@@ -12,8 +12,8 @@ namespace SI
     using _name ## 2 = _name ## _t<detail::vec2<SIdouble>>;                                                         \
     using _name ## 3 = _name ## _t<detail::vec3<SIdouble>>
 
-	// +++ SI BASE DATATYPES +++ 
-	//                               l  m  t  T  c  s  i   (the exponents for length,mass,...)
+	// +++ The 7 SI Base Datatypes +++
+	//                               l  m  t  T  c  s  i   (exponents for length,mass,time,..)
 	DATATYPE(length,                 1, 0, 0, 0, 0, 0, 0); // in meters
 	DATATYPE(mass,                   0, 1, 0, 0, 0, 0, 0); // in kilograms
 	DATATYPE(time,                   0, 0, 1, 0, 0, 0, 0); // in seconds
@@ -22,7 +22,7 @@ namespace SI
 	DATATYPE(amount_of_substance,    0, 0, 0, 0, 0, 1, 0); // in moles
 	DATATYPE(luminous_intensity,     0, 0, 0, 0, 0, 0, 1); // in candelas
 
-	// +++ DERIVED SI DATATYPES +++
+	// +++ The Derived SI Datatypes +++
 	DATATYPE(per_length,            -1, 0,  0, 0, 0, 0, 0); // per meter (reciprocal)
 	DATATYPE(area,                   2, 0,  0, 0, 0, 0, 0); // in square meters
 	DATATYPE(per_area,              -2, 0,  0, 0, 0, 0, 0); // per square meter (reciprocal)
