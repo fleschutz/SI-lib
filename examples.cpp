f9ca1e09b14885958456dd6094bc680d0a7f735a
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-09-05 13:56:41 +0200

    Improved example #17

--- examples.cpp
+++ examples.cpp
@@ -104,9 +104,12 @@ int main() {
     println(glide_path);
 
 
-    print("17. What's the min speed required to escape from the Moon? ");
-    auto escape_speed = formula::gravitational_escape_velocity(dataset::Moon.mass, dataset::Moon.mean_radius);
-    println(escape_speed, " or ", to_equivalent(escape_speed));
+    print("17. What's the min speed required to escape from the planets? ");
+    for (auto& planet : dataset::planets) {
+        auto min_speed = formula::gravitational_escape_velocity(planet.mass, planet.diameter / 2.0);
+        print(planet.name, "=", min_speed, " or ", to_equivalent(min_speed), ", ");
+    }
+    println("");
 
 
     print("18. What's the windchill temperature of 5°C air temperature at 55km/h wind? ");
