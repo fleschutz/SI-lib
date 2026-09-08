b685a457fbbad40ed8d461a3902247af93c8872e
Markus Fleschutz <markus.fleschutz@atcsim.de>
2026-09-08 15:07:49 +0200

    Added Doxygen syntax

--- SI/all.h
+++ SI/all.h
@@ -1,13 +1,14 @@
-// USAGE:       #include <SI/all.h>
-// DESCRIPTION: Header file to include all header files for convenience.
+/// @file	all.h
+/// @brief	Header file for convenience to include all header files of libSI.
+/// @details	Add '#include <SI/all.h>' in your source code to use it.
 
 #pragma once
 
-#include "datatypes.h"    // <-- datatypes such as SI::length
-#include "units.h"        // <-- units such as SI::meters
-#include "literals.h"     // <-- literals such as 100_m
-#include "constants.h"    // <-- constants such as SI::constant::speed_of_light
-#include "formulas.h"     // <-- formula functions such as SI::formula::wavelength()
-#include "conversions.h"  // <-- conversion functions such as SI::to_string()
-#include "print.h"        // <-- simple print to console functions 
-#include "tests.h"        // <-- unit tests at compile-time to verify everything
+#include "datatypes.h"    /// <-- datatypes such as SI::length
+#include "units.h"        /// <-- units such as SI::meters
+#include "literals.h"     /// <-- literals such as 100_m
+#include "constants.h"    /// <-- constants such as SI::constant::speed_of_light
+#include "formulas.h"     /// <-- formula functions such as SI::formula::wavelength()
+#include "conversions.h"  /// <-- conversion functions such as SI::to_string()
+#include "print.h"        /// <-- simple print to console functions 
+#include "tests.h"        /// <-- unit tests at compile-time to verify everything
